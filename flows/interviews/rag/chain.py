f8b53a755d4eb1d09317020dd2b28339c8f96707
EunJiJung <bianbbc87@gmail.com>
2025-05-31 15:07:24 +0900

    refactor/#3 : 챕터 질문 종료와 호응 프롬프팅 추가
    Signed-off-by: EunJiJung <bianbbc87@gmail.com>

--- flows/interviews/rag/chain.py
+++ flows/interviews/rag/chain.py
@@ -1,17 +1,14 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..')))
 from flows.interviews.data.load_and_split import ensure_split_documents
 
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.runnables import RunnableLambda, RunnableSequence, RunnableWithMessageHistory
+from langchain_core.runnables import RunnableLambda, RunnableWithMessageHistory
 from operator import itemgetter
 
 # 초기화 시 split_documents.pkl 생성
 ensure_split_documents()
 
 from .retriever import get_retriever
-from .prompt import initial_prompt, chapter_prompt
+from .prompt import initial_prompt, followup_prompt
 from .llm import get_llm
 from .memory import get_session_history
 
@@ -21,14 +18,14 @@ def build_rag_chain():
 
     # LangChain 최신 방식: LLMChain 대신 prompt | llm
     initial_chain = initial_prompt | llm
-    chapter_chain = chapter_prompt | llm
+    followup_chain = followup_prompt | llm
 
     # 라우팅 함수: is_first에 따라 initial 또는 chapter 체인 선택
     def router_func(inputs):
         if inputs.get("is_first", True):
             return initial_chain
         else:
-            return chapter_chain
+            return followup_chain
 
     router_runnable = RunnableLambda(router_func)
 
